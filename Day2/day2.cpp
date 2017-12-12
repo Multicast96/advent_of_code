13db2cd629f1ed2ad0e8eb3bdcdfa0485447f8d4
Multicast96 <kamil.dakus@gmail.com>
2017-12-12 22:18:47 +0100

    day2 part two

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -8,12 +8,15 @@
 using namespace std;
 
 int checksum;
+int partTwoSum;
 int tmp;
 ifstream file;
 string line;
 stringstream stringStream;
 vector<int> lineValues;
 pair<vector<int>::iterator, vector<int>::iterator> MinMax;
+
+int PartTwo(vector<int>&);
 
 int main() {
 	file.open("input.txt");
@@ -23,11 +26,23 @@ int main() {
 		MinMax = minmax_element(lineValues.begin(), lineValues.end());
 		checksum += *MinMax.second - *MinMax.first;
 
+		partTwoSum += PartTwo(lineValues);
+
 		lineValues.clear();
 		stringStream.clear();
 	}
 	file.close();
 	cout << checksum << endl;
+	cout << partTwoSum << endl;
 	system("pause");
 	return 0;
+}
+
+int PartTwo(vector<int> &lineValues) {
+	for (vector<int>::iterator it = lineValues.begin(); it != lineValues.end(); it++){
+		for (vector<int>::iterator it2 = it+1; it2 != lineValues.end(); it2++){
+			if (*it % (*it2) == 0) return *it / (*it2);
+			if (*it2%(*it) == 0) return *it2 / (*it);
+		}
+	}
 }
