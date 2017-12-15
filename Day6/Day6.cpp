fd0a205a8fcee2f07f78ae38ba3c712bbc13d2f0
Multicast96 <kamil.dakus@gmail.com>
2017-12-15 16:28:45 +0100

    Day 6|Part One|Part Two|

--- Day6/Day6.cpp
+++ Day6/Day6.cpp
@@ -5,14 +5,18 @@
 
 using namespace std;
 
-//vector<unsigned long long> banks_archive;
-set<unsigned long long>::iterator archive_it;
-vector<int> banks = { 14,0,15,12,11,11,3,5,1,6,8,4,9,1,8,4 };
-//vector<int> banks = { 0,2,7,0 };
-vector<int>::iterator it;
+vector<unsigned long long> banks_archive2;
+vector<unsigned long long>::iterator archive_it2;
+
 set<unsigned long long> banks_archive;
+set<unsigned long long>::iterator archive_it;
+
+vector<int> banks = { 14,0,15,12,11,11,3,5,1,6,8,4,9,1,8,4 };
+vector<int>::iterator it;
+
 int valueToRedistribute;
 int cycles;
+int cyclesInLoop;
 
 unsigned long long BankHash(vector<int> &);
 
@@ -20,6 +24,7 @@ int main() {
 	while (true){
 		//hash from data banks
 		banks_archive.insert(BankHash(banks));
+		banks_archive2.push_back(BankHash(banks));
 
 		//finding max then max redistribution
 		it = max_element(banks.begin(), banks.end());
@@ -33,16 +38,21 @@ int main() {
 		cycles++;
 		archive_it = banks_archive.find(BankHash(banks));
 		if (archive_it != banks_archive.end()) {
+			//PART TWO
+			archive_it2 = find(banks_archive2.begin(), banks_archive2.end(), BankHash(banks));
+			cyclesInLoop = distance(archive_it2, banks_archive2.end());
+
 			cout << "Duplicated hash: "<<BankHash(banks) << endl;
 			cout << "Data banks: ";
-			for (int a : banks) {
-				cout << a << " ";
+			for (int &value : banks) {
+				cout << value << " ";
 			}
 			cout << endl;
 			break;
 		}
 	}
 	cout <<"Cycles: "<<cycles << endl;
+	cout << "Cycles in loop: "<<cyclesInLoop << endl;
 	system("pause");
 	return 0;
 }
